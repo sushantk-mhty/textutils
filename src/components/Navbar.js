69f22c8d7dcb597ef27bf8921f36ac3cb9485e66
sushantk-mhty <sushant.mhty@outlook.com>
2022-11-21 10:32:20 +0530

    add video 13

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -32,7 +34,26 @@ export default function Navbar(props) {
               </a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              onClick={props.toggleMode}
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable Dark Mode
+            </label>
+          </div>
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
@@ -42,7 +63,7 @@ export default function Navbar(props) {
             <button className="btn btn-primary" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </div>
     </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,10 +4,29 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to upper case!", "success");
   };
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lower case!", "success");
+  };
+  const handleClearClick = () => {
+    setText("");
+    props.showAlert("Text cleared!", "success");
+  };
+
+  const handleCopyClick = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard!", "success");
+  };
+
+  const handleExtraSpaceClick = () => {
+    let newTxt = text.split(/[ ]+/);
+    setText(newTxt.join(" "));
   };
   const handleOnChange = (event) => {
     setText(event.target.value);
@@ -16,11 +35,18 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h1>{props.heading} </h1>
         <div className="mb-3">
           <textarea
             className="form-control"
+            style={{
+              backgroudColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "black" : "#042743",
+            }}
             value={text}
             onChange={handleOnChange}
             id="myBox"
@@ -33,15 +59,34 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>
           Convert to lowercase
         </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary mx-1"
+          onClick={handleExtraSpaceClick}
+        >
+          Remove Space
+        </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h1>Your text summary</h1>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </p>
       </div>
     </>
   );
