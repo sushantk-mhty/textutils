74eeb955bfadb350f4072f9bcb1f5be8e7de9584
sushantk-mhty <sushant.mhty@outlook.com>
2022-11-24 12:55:26 +0530

    Video 15 completed

--- src/App.js
+++ src/App.js
@@ -22,10 +22,18 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enclosed", "success");
+      document.title = "TextUtils - Dark Mode";
+      setInterval(() => {
+        document.title = "TextUtils is amazing";
+      }, 2000);
+      setInterval(() => {
+        document.title = "Install TextUtils is now";
+      }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enclosed", "primary");
+      document.title = "TextUtils - Light Mode";
     }
   };
   return (
